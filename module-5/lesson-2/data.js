252151938a3b015d69ac93838c503cb5da9033bb
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-13 11:28:57 +0300

    Lesson 2 created

---
+++ module-5/lesson-2/data.js
@@ -0,0 +1,19 @@
+export const customers = {
+    id: 1,
+    name: "Ahmet",
+    isActive: true,
+    subscriptions: [
+        {
+            id: 101,
+            planId: 5,
+            status: "active",
+            monthlyPrice: 700
+        },
+        {
+            id: 102,
+            planId: 10,
+            status: "paused",
+            monthlyPrice: 1500
+        }
+    ]
+};
