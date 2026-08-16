3bbd0d7014eb0368b092d57381e68030cf78cb05
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-16 22:09:51 +0300

    Lesson 2 fixed commit

--- module-5/lesson-2/lesson-2.js
+++ module-5/lesson-2/lesson-2.js
@@ -1,27 +1,38 @@
 import { customers } from "./data.js"
 
 function cancelSubscription(customer, subscriptionId) {
-    if(!customer) {
-        throw new Error('Customer is required')
+    if (!customer) {
+        throw new Error("Customer is required");
     }
-    if(typeof subscriptionId !== 'number') {
-        throw new Error('subscriptionId must be number')
+
+    if (typeof subscriptionId !== "number") {
+        throw new Error("subscriptionId must be number");
     }
-    if(!customer.isActive) {
-        throw new Error('customer must be active')
+
+    if (!customer.isActive) {
+        throw new Error("Customer must be active");
     }
-    if(customer.subscriptions === undefined || customer.subscriptions.length === 0) {
-        throw new Error('customer subscriptions must be available and length bigger than 0')
+
+    if (
+        customer.subscriptions === undefined ||
+        customer.subscriptions.length === 0
+    ) {
+        throw new Error("Customer must have subscriptions");
     }
-    for(const subs of customer.subscriptions) {
-        if(subs.id !== subscriptionId) {
-            throw new Error('subscription id must be subscriptionId ')
-        }
-        if(subs.status === 'cancelled') {
-            continue
-        }
-        subs.status = 'cancelled'
-        const cancelledSubscription = cancelSubscription(customer, 102)
-        console.log(cancelSubscription);
+
+    const targetSubscription = customer.subscriptions.find(
+        subs => subs.id === subscriptionId
+    );
+
+    if (!targetSubscription) {
+        throw new Error("Subscription not found");
     }
+
+    if (targetSubscription.status === "cancelled") {
+        throw new Error("Subscription is already cancelled");
+    }
+
+    targetSubscription.status = "cancelled";
+
+    return targetSubscription;
 }
