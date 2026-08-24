9233e34ab5482a80edca5363c741a1b8d207554e
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-24 19:47:00 +0300

    i doing practice after 11 days

---
+++ module-5/practice.js
@@ -0,0 +1,28 @@
+const customer = {
+    id: 10,
+    isActive: true,
+    subscriptions: [
+        { id: 101, status: 'active', monthlyPrice: 500 },
+        { id: 102, status: 'paused', monthlyPrice: 1800 },
+        { id: 103, status: 'active', monthlyPrice: 900 }
+    ]
+}
+function cancelSubscription(customer, subscriptionId) {
+    if(!customer) {
+        throw new Error('customer must be available')
+    }
+    if(!(typeof subscriptionId === 'number')) {
+        throw new Error('subscriptionId type must be number')
+    }
+    if(!customer.isActive) {
+        throw new Error('Customer must be active')
+    }
+    if((customer.subscriptions ?? []).length === 0) {
+        throw new Error('Subscriptions must be available')
+    }
+    const targetSub = customer.subscriptions.find(sub => sub.id === subscriptionId)
+    if(!targetSub) {
+        throw new Error('Target subscription not found')
+    }
+    return targetSub
+}
