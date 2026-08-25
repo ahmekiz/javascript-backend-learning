d19b81fe4ad090fd90b5f97647ae32cd6281f8dc
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-25 18:36:59 +0300

    lesson-3 Helper Exercise completed

--- module-5/lesson-3/lesson-3.js
+++ module-5/lesson-3/lesson-3.js
@@ -172,3 +172,44 @@ function recalculateSubscriptionPrice(customer, subscriptionId) {
         totalMonthlyPrice: targetSub.totalMonthlyPrice
     }
 }
+
+//Helper exercise
+function findSubscription(subscriptions, subscriptionId) {
+    return subscriptions.find(sub => sub.id === subscriptionId)
+}
+
+function findPlan(plans, planId) {
+    return plans.find(plan => plan.id === planId)
+}
+
+//refactor deactivePlan Function
+function deactivatePlanRefactored(customer, subscriptionId, planId) {
+    if(!customer) {
+        throw new Error('Customer not found')
+    }
+    if(typeof subscriptionId !== 'number' || typeof planId !== 'number') {
+        throw new Error('subscriptionId and planId types must be number')
+    }
+    if((customer.subscriptions ?? []).length === 0) {
+        throw new Error('subscriptions must be available')
+    }
+    const targetSub = findSubscription(customer.subscriptions, subscriptionId)
+    if(!targetSub) {
+        throw new Error('Target Subscription not found')
+    }
+    if(targetSub.status === 'cancelled') {
+        throw new Error('This subscription not available')
+    }
+    if((targetSub.plans ?? []).length === 0) {
+        throw new Error('Plans must be available')
+    }
+    const targetPlan = findPlan(targetSub.plans, planId)
+    if(!targetPlan) {
+        throw new Error('Target Plan not found')
+    }
+    if(!targetPlan.isActive) {
+        throw new Error('Plan is already inactive')
+    }
+    targetPlan.isActive = false
+    return targetPlan
+}
