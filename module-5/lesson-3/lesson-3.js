6feb86b0bdee689560f6feb00746d0764537c122
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-25 17:44:39 +0300

    lesson-3 3.practice finished

--- module-5/lesson-3/lesson-3.js
+++ module-5/lesson-3/lesson-3.js
@@ -67,3 +67,53 @@ function updatePlanPrice(customer, subscriptionId, planId, newPrice) {
     targetPlan.monthlyPrice = newPrice
     return targetPlan
 }
+
+function applyPlanDiscount(customer, subscriptionId, planId, discountPercent) {
+    if(!customer) {
+        throw new Error('Customer not found')
+    }
+    if(typeof subscriptionId !== 'number' || typeof planId !== 'number' || typeof discountPercent !== 'number') {
+        throw new Error('subscriptionId, planId and discountpercent type must be number')
+    }
+    if(discountPercent <= 0 || discountPercent > 30) {
+        throw new Error('discountPercent not available')
+    }
+    if((customer.subscriptions ?? []).length === 0) {
+        throw new Error('Subscriptions not available')
+    }
+    if(!customer.isActive) {
+        throw new Error('customer must be active')
+    }
+    const targetSub = customer.subscriptions.find(sub => sub.id === subscriptionId)
+    if(!targetSub) {
+        throw new Error('target subscription not found')
+    }
+    if(targetSub.status !== 'active') {
+        throw new Error('target subscription status must be active')
+    }
+    if((targetSub.plans ?? []).length === 0) {
+        throw new Error('Plans not available')
+    }
+    const targetPlan = targetSub.plans.find(plan => plan.id === planId)
+    if(!targetPlan) {
+        throw new Error('target plan not found')
+    }
+    if(!targetPlan.isActive) {
+        throw new Error('target plan must be active')
+    }
+    const oldPrice = targetPlan.monthlyPrice
+    const calculation = targetPlan.monthlyPrice - (targetPlan.monthlyPrice * discountPercent / 100)
+    if(calculation < 500) {
+        throw new Error('calculation-value must be bigger than 500 or equal to 500')
+    }
+    targetPlan.monthlyPrice = calculation
+
+    return {
+        subscriptionId: targetSub.id,
+        planId: targetPlan.id,
+        planName: targetPlan.name,
+        oldPrice: oldPrice,
+        newPrice: targetPlan.monthlyPrice,
+        discountPercent: discountPercent
+    }
+}
