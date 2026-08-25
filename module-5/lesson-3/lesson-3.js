996ca15c4ca739505d25aceef29fa2915d6543fe
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-25 16:18:24 +0300

    lesson-3 practice 1 finished

--- module-5/lesson-3/lesson-3.js
+++ module-5/lesson-3/lesson-3.js
@@ -1,2 +1,32 @@
 import { customer } from './data.js'
 
+function deactivatePlan(customer, subscriptionId, planId) {
+    if(!customer) {
+        throw new Error('Customer not found')
+    }
+    if(typeof subscriptionId !== 'number' || typeof planId !== 'number') {
+        throw new Error('subscriptionId and planId types must be number')
+    }
+    if((customer.subscriptions ?? []).length === 0) {
+        throw new Error('subscriptions must be available')
+    }
+    const targetSub = customer.subscriptions.find(sub => sub.id === subscriptionId)
+    if(!targetSub) {
+        throw new Error('Target Subscription not found')
+    }
+    if(targetSub.status === 'cancelled') {
+        throw new Error('This subscription not available')
+    }
+    if((targetSub.plans ?? []).length === 0) {
+        throw new Error('Plans must be available')
+    }
+    const targetPlan = targetSub.plans.find(plan => plan.id === planId)
+    if(!targetPlan) {
+        throw new Error('Target Plan not found')
+    }
+    if(!targetPlan.isActive) {
+        throw new Error('Plan is already inactive')
+    }
+    targetPlan.isActive = false
+    return targetPlan
+}
