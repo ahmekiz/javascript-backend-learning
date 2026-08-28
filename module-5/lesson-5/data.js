017c02be6f2007f0fb8e241db00b82a93a3cae2b
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-08-28 12:20:23 +0300

    lesson-5 files created

---
+++ module-5/lesson-5/data.js
@@ -0,0 +1,24 @@
+export const customer = {
+    id: 12,
+    name: "Mehmet",
+    isActive: true,
+    subscriptions: [
+        {
+            id: 201,
+            status: "active",
+            totalMonthlyPrice: 2600,
+            plans: [
+                { id: 10, name: "Starter", monthlyPrice: 700, isActive: true },
+                { id: 11, name: "Pro", monthlyPrice: 1900, isActive: true }
+            ]
+        },
+        {
+            id: 202,
+            status: "active",
+            totalMonthlyPrice: 2700,
+            plans: [
+                { id: 20, name: "Business", monthlyPrice: 2700, isActive: true }
+            ]
+        }
+    ]
+}
