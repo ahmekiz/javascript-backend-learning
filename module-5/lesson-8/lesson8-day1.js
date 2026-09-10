7b7f66b740772699f73213d427fbbb12b1f62c1b
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-10 16:21:33 +0300

    customer & request added

--- module-5/lesson-8/lesson8-day1.js
+++ module-5/lesson-8/lesson8-day1.js
@@ -13,3 +13,39 @@ function isValidPlan(plan) {
     }
     return true
 }
+
+const customer = {
+    id: 15,
+    subscriptions: [
+        {
+            id: 101,
+            status: "active",
+            plans: [
+                { id: 1, name: " Basic ", price: 300 },
+                null,
+                { id: 7, name: "Pro", price: 900 }
+            ]
+        },
+
+        {
+            id: 102,
+            status: "cancelled",
+            plans: [
+                { id: 8, name: "Ultra", price: 1200 }
+            ]
+        },
+
+        {
+            id: 103,
+            status: "active",
+            plans: "invalid"
+        },
+
+        null
+    ]
+};
+
+const request = {
+    planId: 7
+};
+
