f4ef606cf6fdc6f3b0680d9a1f9103e357f11ded
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-17 23:36:40 +0300

    lesson-9 files created

---
+++ module-5/lesson-9/day1.js
@@ -0,0 +1,20 @@
+const subscriptions = [
+    { id: 101, status: 'active' },
+    { id: 205, status: 'active' },
+    { id: 309, status: 'paused' }
+];
+
+const requests = [
+    { subscriptionId: 101 },
+    { subscriptionId: 205 },
+    { subscriptionId: 101 }
+];
+
+for (const request of requests) {
+
+    // 1. subscription mevcut mu?
+
+    // 2. duplicate request mi?
+
+    // 3. değilse seen Set'e ekle
+}
