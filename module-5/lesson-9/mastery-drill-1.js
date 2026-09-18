ac0e2989d3b316538ece5b1b993043d006c197bd
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-19 01:58:57 +0300

    mastery-drill-1 completed

--- module-5/lesson-9/mastery-drill-1.js
+++ module-5/lesson-9/mastery-drill-1.js
@@ -17,3 +17,38 @@ const invites = [
   { id: 105, userId: 999, roleId: 10 },
   { id: 106, userId: 3, roleId: 20 }
 ];
+
+const userById = new Map();
+for(const user of users) {
+    userById.set(user.id, user)
+}
+
+const roleById = new Map();
+for(const role of roles) {
+    roleById.set(role.id, role)
+}
+
+const userIdSet = new Set();
+const result = {
+    accepted: [],
+    rejected: []
+}
+
+for(const invite of invites) {
+    if(!userById.has(invite.userId)) {
+        result.rejected.push(invite)
+        continue
+    }
+    if(!roleById.has(invite.roleId)) {
+        result.rejected.push(invite)
+        continue
+    }
+    if(userIdSet.has(invite.userId)) {
+        result.rejected.push(invite)
+        continue
+    }
+    const user = userById.get(invite.userId)
+    const role = roleById.get(invite.roleId)
+    result.accepted.push({invite, user, role})
+    userIdSet.add(invite.userId)
+}
