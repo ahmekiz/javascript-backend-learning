80957fae77ed333c1b5380e73789b7519e25bbea
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-19 02:46:38 +0300

    drill-4 files created

---
+++ module-5/lesson-9/mastery-drill-4.js
@@ -0,0 +1,21 @@
+const customers = [
+    { id: 10, name: 'Ahmet' },
+    { id: 20, name: 'Mehmet' },
+    { id: 30, name: 'Zeynep' },
+    { id: 40, name: 'Ayşe' }
+];
+
+const products = [
+    { id: 100, name: 'Keyboard', stock: 5 },
+    { id: 200, name: 'Mouse', stock: 8 }
+];
+
+const requests = [
+    { id: 1, customerId: 10, productId: 100, quantity: 3 },
+    { id: 2, customerId: 20, productId: 100, quantity: 3 },
+    { id: 3, customerId: 20, productId: 200, quantity: 4 },
+    { id: 4, customerId: 10, productId: 200, quantity: 2 },
+    { id: 5, customerId: 30, productId: 999, quantity: 2 },
+    { id: 6, customerId: 30, productId: 200, quantity: 3 },
+    { id: 7, customerId: 40, productId: 200, quantity: 2 }
+];
