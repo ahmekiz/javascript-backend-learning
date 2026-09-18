3fa80a1c19946801bf577a9f8a489d547db3a49e
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-19 01:06:35 +0300

    cross-collection-2 completed

--- module-5/lesson-9/cross-collection-2.js
+++ module-5/lesson-9/cross-collection-2.js
@@ -11,3 +11,29 @@ const commands = [
     { id: 4, deviceId: 999, type: 'ON' },
     { id: 5, deviceId: 303, type: 'COLOR' }
 ];
+
+const deviceById = new Map();
+for(const device of devices) {
+    deviceById.set(device.id, device)
+}
+
+const dtoSet = new Set();
+const result = {
+    accepted: [],
+    rejected: []
+}
+for(const command of commands) {
+    if(!deviceById.has(command.deviceId)) {
+        result.rejected.push(command)
+        continue
+    }
+    if(dtoSet.has(command.deviceId)) {
+        result.rejected.push(command)
+        continue
+    }
+    dtoSet.add(command.deviceId)
+    result.accepted.push({
+        command: command,
+        device: deviceById.get(command.deviceId)
+    })
+}
