16b666b30c309b50645c0d13a8cb5ce7a8b00035
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-18 18:44:30 +0300

    structure-aggregation final files created

---
+++ module-5/lesson-9/structure-aggregation-final.js
@@ -0,0 +1,9 @@
+const telemetry = [
+  { id: 1, deviceId: 100, temperature: 24 },
+  { id: 2, deviceId: 200, temperature: 30 },
+  { id: 3, deviceId: 100, temperature: 28 },
+  { id: 4, deviceId: 100, temperature: 26 },
+  { id: 5, deviceId: 200, temperature: 32 }
+];
+
+const telemetryByDeviceId = new Map();
