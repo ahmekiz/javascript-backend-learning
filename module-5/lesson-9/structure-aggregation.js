8530c5627c3e2aee40e46b86fff62e760329b724
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-18 18:29:32 +0300

    lesson-9 structure-aggregation created

---
+++ module-5/lesson-9/structure-aggregation.js
@@ -0,0 +1,8 @@
+const orders = [
+  { id: 1, customerId: 10, total: 100 },
+  { id: 2, customerId: 20, total: 250 },
+  { id: 3, customerId: 10, total: 400 },
+  { id: 4, customerId: 20, total: 50 },
+  { id: 5, customerId: 30, total: 300 }
+];
+
