e6e5fd303f844740a1154d7af78f0c1c7368c30d
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-19 18:29:11 +0300

    mastery-1 files created

---
+++ module-5/lesson-9/mastery-1.js
@@ -0,0 +1,11 @@
+const products = [
+    { id: 1, name: 'Keyboard', stock: 8 },
+    { id: 2, name: 'Mouse', stock: 5 },
+    { id: 3, name: 'Monitor', stock: 3 }
+];
+
+const requests = [
+    { orderId: 101, productId: 1, quantity: 3 },
+    { orderId: 102, productId: 2, quantity: 2 },
+    { orderId: 103, productId: 1, quantity: 4 }
+];
