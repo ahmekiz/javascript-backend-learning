d2b62ebf7a89ebef6d42ecf71e99bd8458cba711
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-19 04:40:08 +0300

    My Perfect Solution

--- module-5/lesson-9/mastery-drill-5.js
+++ module-5/lesson-9/mastery-drill-5.js
@@ -21,3 +21,59 @@ const requests = [
     { id: 8, customerId: 40, productId: 200, quantity: 1 },
     { id: 9, customerId: 999, productId: 100, quantity: 1 }
 ];
+
+const customerById = new Map()
+const totalProductPrice = new Map()
+for(const customer of customers) {
+    customerById.set(customer.id,customer)
+    totalProductPrice.set(customer.id, 0)
+}
+
+const productById = new Map()
+const acceptedProduct = new Map()
+for(const product of products) {
+    productById.set(product.id, product)
+    acceptedProduct.set(product.id, 0)
+}
+
+const customerIdSet = new Set()
+const result = {
+    accepted: [],
+    rejected: []
+}
+for(const req of requests) {
+    if(!customerById.has(req.customerId)) {
+        result.rejected.push(req)
+        continue
+    }
+    if(!productById.has(req.productId)) {
+        result.rejected.push(req)
+        continue
+    }
+    if(customerIdSet.has(req.customerId)) {
+        result.rejected.push(req)
+        continue
+    }
+    const customer = customerById.get(req.customerId)
+    const product = productById.get(req.productId)
+    const productPrice = product.price * req.quantity
+    const projectedProductPrice = totalProductPrice.get(req.customerId) + productPrice
+    const projectedProductStock = acceptedProduct.get(req.productId) + req.quantity
+    if(projectedProductPrice > customer.credit) {
+        result.rejected.push(req)
+        continue
+    }
+    if(projectedProductStock > product.stock) {
+        result.rejected.push(req)
+        continue
+    }
+    customerIdSet.add(req.customerId)
+    totalProductPrice.set(req.customerId, projectedProductPrice)
+    acceptedProduct.set(req.productId, projectedProductStock)
+    result.accepted.push({
+        request: req,
+        customer,
+        product,
+        totalCost: totalProductPrice.get(req.customerId)
+    })
+}
