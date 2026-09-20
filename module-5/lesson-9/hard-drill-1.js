7ecbd20cb392404b09853c5abf6fb545846bf3f3
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-21 02:31:42 +0300

    hard-drill-1.js files created

--- module-5/lesson-9/hard-drill-1.js
+++ module-5/lesson-9/hard-drill-1.js
@@ -1,22 +1,33 @@
 const developers = [
-    { id: 1, name: 'Ada',   maxHours: 10, currentHours: 4 },
-    { id: 2, name: 'Linus', maxHours: 8,  currentHours: 6 },
-    { id: 3, name: 'Grace', maxHours: 12, currentHours: 8 }
+    { id: 1, name: 'Ada',   currentHours: 5, maxHours: 10 },
+    { id: 2, name: 'Linus', currentHours: 7, maxHours: 9 },
+    { id: 3, name: 'Grace', currentHours: 1, maxHours: 10 }
 ];
 
 const projects = [
-    { id: 10, name: 'API',       budgetHours: 10, usedHours: 3 },
-    { id: 20, name: 'Dashboard', budgetHours: 6,  usedHours: 5 },
-    { id: 30, name: 'IoT',       budgetHours: 8,  usedHours: 2 }
+    { id: 10, name: 'API',  usedHours: 4, budgetHours: 8 },
+    { id: 20, name: 'Auth', usedHours: 6, budgetHours: 10 }
 ];
 
 const requests = [
-    { assignmentId: 1001, developerId: 1, projectId: 10, hours: 3 },
-    { assignmentId: 1002, developerId: 2, projectId: 10, hours: 3 },
-    { assignmentId: 1002, developerId: 3, projectId: 30, hours: 2 },
-    { assignmentId: 1003, developerId: 1, projectId: 20, hours: 2 },
-    { assignmentId: 1004, developerId: 1, projectId: 10, hours: 3 },
-    { assignmentId: 1005, developerId: 3, projectId: 30, hours: 3 },
-    { assignmentId: 1006, developerId: 2, projectId: 20, hours: 1 },
-    { assignmentId: 1001, developerId: 2, projectId: 30, hours: 1 }
+    { assignmentId: 4001, developerId: 1, projectId: 10, hours: 2 },
+    { assignmentId: 4002, developerId: 2, projectId: 20, hours: 3 },
+    { assignmentId: 4003, developerId: 3, projectId: 10, hours: 2 },
+    { assignmentId: 4000, developerId: 3, projectId: 20, hours: 1 },
+    { assignmentId: 4004, developerId: 3, projectId: 20, hours: 2 }
 ];
+
+const batchState = {
+    seenAssignmentIds: new Set([4000]),
+
+    addedHoursByDeveloper: new Map([
+        [1, 2],
+        [2, 0],
+        [3, 1]
+    ]),
+
+    addedHoursByProject: new Map([
+        [10, 1],
+        [20, 1]
+    ])
+};
