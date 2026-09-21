1145c86c0f84d1eda5ded9f2bfc04852c9c5af2f
Ahmet Ekiz <ahmetekizofficial@gmail.com>
2026-09-21 20:47:01 +0300

    hard-drill-2 files created

--- module-5/lesson-9/hard-drill-2.js
+++ module-5/lesson-9/hard-drill-2.js
@@ -1,44 +1,33 @@
-const developer = {
-    id: 1,
-    name: 'Ada',
-    currentHours: 5,
-    maxHours: 12
-};
+const developers = [
+    { id: 1, name: 'Ada',   currentHours: 5, maxHours: 10 },
+    { id: 2, name: 'Linus', currentHours: 7, maxHours: 9 },
+    { id: 3, name: 'Grace', currentHours: 1, maxHours: 10 }
+];
 
-const project = {
-    id: 10,
-    name: 'API',
-    usedHours: 4,
-    budgetHours: 10
-};
+const projects = [
+    { id: 10, name: 'API',  usedHours: 4, budgetHours: 8 },
+    { id: 20, name: 'Auth', usedHours: 6, budgetHours: 10 }
+];
+
+const requests = [
+    { assignmentId: 4001, developerId: 1, projectId: 10, hours: 2 },
+    { assignmentId: 4002, developerId: 2, projectId: 20, hours: 3 },
+    { assignmentId: 4003, developerId: 3, projectId: 10, hours: 2 },
+    { assignmentId: 4000, developerId: 3, projectId: 20, hours: 1 },
+    { assignmentId: 4004, developerId: 3, projectId: 20, hours: 2 }
+];
 
 const batchState = {
     seenAssignmentIds: new Set([4000]),
 
     addedHoursByDeveloper: new Map([
-        [1, 2]
+        [1, 2],
+        [2, 0],
+        [3, 1]
     ]),
 
     addedHoursByProject: new Map([
-        [10, 1]
+        [10, 1],
+        [20, 1]
     ])
 };
-
-const request = {
-    assignmentId: 4001,
-    developerId: 1,
-    projectId: 10,
-    hours: 3
-};
-
-const workingSeenAssignmentIds =
-    new Set(batchState.seenAssignmentIds)
-
-const workingAddedHoursByDeveloper =
-    new Map(batchState.addedHoursByDeveloper)
-
-const workingAddedHoursByProject =
-    new Map(batchState.addedHoursByProject)
-
-const newHoursByDeveloper = new Map()
-const newHoursByProject = new Map()
